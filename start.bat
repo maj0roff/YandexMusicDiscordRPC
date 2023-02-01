82deafb69b801058ea245a1583c06e0aae977004
Александр <54349566+maj0roff@users.noreply.github.com>
2023-02-01 23:08:34 +0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,7 +1,4 @@
 @echo off
 :restart
-echo ---------------------- START (%DATE:~0,2%.%DATE:~3,2% %TIME:~0,8%) ----------------------
 py main.py
-echo ---------------------- STOP (%DATE:~0,2%.%DATE:~3,2% %TIME:~0,8%) ----------------------
-ping -n 1 127.0.0.1 > NUL
 goto restart
