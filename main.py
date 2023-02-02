f08985fe8e18ef77137c06dd7beef74b6ed534d2
Александр <minecrfsoft@gmail.com>
2023-02-02 23:33:13 +0500

    11

--- main.py
+++ main.py
@@ -18,25 +18,6 @@ import time
 from modules.rpc import MRPC
 from modules.yandexmusic import MYAPI
 from threading import Thread
-import keyboard
-import win32gui, win32con
-
-isHidden = 0
-def windowControl_HideShow():
-    hwnd = win32gui.GetForegroundWindow()
-    global isHidden
-    if isHidden == 0:
-        isHidden = 1
-        #print(isHidden)
-        win32gui.ShowWindow(hwnd , win32con.SW_HIDE)
-    elif isHidden == 1:
-        isHidden = 0
-        #print(isHidden)
-        win32gui.ShowWindow(hwnd , win32con.SW_SHOW)
-    
-def windowControl():
-    keyboard.add_hotkey("ctrl+f9", windowControl_HideShow)
-        
 
 def app():
     print("[Яндекс Музыка] Для того что-бы скрыть или показать окно консоли нажмите Ctrl+F9")
@@ -63,9 +44,5 @@ def app():
 
 if __name__ == "__main__":
     t1 = Thread(target=app)
-    t2 = Thread(target=windowControl)
-
     t1.start()
-    t2.start()
     t1.join()
-    t2.join()
