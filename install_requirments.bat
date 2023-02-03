95c383f76c4950a51a3f76f1a9c26334f797187e
Александр <54349566+maj0roff@users.noreply.github.com>
2023-02-03 20:52:30 +0500

    Delete install_requirments.bat

--- install_requirments.bat
+++
@@ -1,8 +0,0 @@
-pip install yandex-music --upgrade
-pip install selenium
-pip install pypresence
-pip install yandex_music
-pip install webdriver_manager
-pip install keyboard
-pip install pywin32
-pip install win32co
